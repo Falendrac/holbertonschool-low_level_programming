9def0406d9b4eed309cb37fdb0e2ebb0e325d579
Falendrac <leger_franck@hotmail.fr>
2021-09-30 08:43:37 +0000

    Modify comment for betty like

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-/*Program we use printf to print a little text */
+/**
+ * main - print a text with the function printf
+ *
+ * Return: 0
+ */
 
 int main(void)
 {
-	/* Print a text with the function printf */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
