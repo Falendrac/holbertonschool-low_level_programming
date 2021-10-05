fe696d6f69fd992707efb05ee735582ba776cbdc
Falendrac <leger_franck@hotmail.fr>
2021-10-05 14:01:53 +0000

    Add source code 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Computes and prints the sum of all the multiples of 3 or 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int multi3, multi5;
+
+	for (multi3 = 3; multi3 < 1024; multi3 += 3)
+	{
+	}
+
+	printf("%d\n", multi3);
+
+	for (multi5 = 5; multi5 < 1024; multi5 += 5)
+	{
+	}
+
+	printf("%d\n", multi5);
+
+	return (0);
+}
