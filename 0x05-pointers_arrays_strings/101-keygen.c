160ac65873e163f330a09c833329c861ec88136c
Falendrac <leger_franck@hotmail.fr>
2021-10-12 17:47:37 +0000

    Add code source 101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - The keygen for crackme
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count = 2772;
+	char gen;
+
+	srand(time(NULL));
+
+	while (count > 126)
+	{
+		gen = rand() % 126;
+		count -= gen;
+		putchar(gen);
+	}
+
+	putchar(count);
+	return (0);
+}
+
