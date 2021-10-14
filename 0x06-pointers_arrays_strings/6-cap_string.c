2a0e7fd0f312a9109d709dedf4c304c7e9374676
Falendrac <leger_franck@hotmail.fr>
2021-10-14 09:38:56 +0000

    Rewrite code source 6-cap_string.c to not use switch

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,33 +9,20 @@
  */
 char *cap_string(char *str)
 {
-	int loop, length;
+	int loop, loopsep, length;
+	char sep[] = { 32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 42, '{', '}', '\0' };
 
 	length = _strlen(str);
 
 	if (length != 0)
 		for (loop = 0; loop < length; loop++)
 			if (*(str + loop) >= 97 && *(str + loop) <= 122)
-				switch (*(str + loop - 1))
-				{
-					case ' ':
-					case '\t':
-					case '\n':
-					case ',':
-					case ';':
-					case '.':
-					case '!':
-					case '?':
-					case '"':
-					case '(':
-					case ')':
-					case '{':
-					case '}':
+				for (loopsep = 0; sep[loopsep] != '\0'; loopsep++)
+					if (*(str + loop - 1) == sep[loopsep])
+					{
 						*(str + loop) -= 32;
 						break;
-					default:
-						break;
-				}
+					}
 
 	return (str);
 }
