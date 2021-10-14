296a2db0bdf871e2b6ee38fe6dd525fd743256bd
Falendrac <leger_franck@hotmail.fr>
2021-10-14 09:17:36 +0000

    Fix delete strlen function and rewrite arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,14 +9,12 @@
  */
 char *leet(char *str)
 {
-	int length, loop1, loop2;
-	char upperc[6] = { 'A', 'E', 'O', 'T', 'L', '\0' };
-	char lowerc[6] = { 'a', 'e', 'o', 't', 'l', '\0' };
-	char number[6] = { '4', '3', '0', '7', '1', '\0' };
+	int loop1, loop2;
+	char upperc[6] = { "AEOTL\0" };
+	char lowerc[6] = { "aeotl\0" };
+	char number[6] = { "43071\0" };
 
-	length = _strlen(str);
-
-	for (loop1 = 0; loop1 < length; loop1++)
+	for (loop1 = 0; *(str + loop1) != '\0'; loop1++)
 		for (loop2 = 0; loop2 < 5; loop2++)
 			if (*(str + loop1) == upperc[loop2] || *(str + loop1) == lowerc[loop2])
 				*(str + loop1) = number[loop2];
@@ -24,22 +22,3 @@ char *leet(char *str)
 	return (str);
 }
 
-/**
- * _strlen - Return the length of a string
- *
- * @s: The String we want to know the length
- *
- * Return: length
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		length++;
-	}
-
-	return (length);
-}
