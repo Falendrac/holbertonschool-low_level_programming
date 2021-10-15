a57e0e5d5501de3f82c21ee9dc6b623aaa0a9072
Falendrac <leger_franck@hotmail.fr>
2021-10-15 14:52:54 +0000

    Fix: Reverse the string and reverse again

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void swap_char(char *a, char *b);
+
 /**
  * infinite_add - Adds two numbers
  *
@@ -12,7 +14,7 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int loop, result = 0, n1_length, n2_length, length_max;
+	int loop, result = 0, n1_length, n2_length, length_max, max;
 
 	n1_length = _strlen(n1);
 	n2_length = _strlen(n2);
@@ -27,9 +29,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	if (size_r != 0 && length_max < size_r - 1)
 	{
-		*(r + length_max + 1) = '\0';
-
-		for (loop = length_max; loop >= 0; loop--, n1_length--, n2_length--)
+		for (loop = 0; loop < length_max; loop++, n1_length--, n2_length--)
 		{
 			if (n1_length >= 0 && n2_length >= 0)
 				result += *(n1 + n1_length) - 48 + *(n2 + n2_length) - 48;
@@ -43,9 +43,21 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			*(r + loop) = result % 10 + 48;
 			result /= 10;
 		}
+
+		if (result != 0)
+		{
+			*(r + length_max) = result + 48;
+			length_max++;
+			*(r + length_max) = '\0';
+		}
+		else
+			*(r + length_max) = '\0';
 	}
 	else
 		return (0);
+
+	for (loop = 0, max = length_max - 1; loop < max; loop++, max--)
+		swap_char((r + loop), (r + max));
 
 	return (r);
 }
@@ -70,3 +82,16 @@ int _strlen(char *s)
 	return (length);
 }
 
+/**
+ * swap_char - Take two point of char and swap the value of the variables
+ * @a: Pointer of a
+ * @b: Pointer of b
+ */
+void swap_char(char *a, char *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
