fa098439bab2a130ef6ea73ebdcc55042718b782
Falendrac <leger_franck@hotmail.fr>
2021-10-18 07:58:05 +0000

    Add code source 2-strchr.c to 0x07 directory

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+ * _strchr - Locates a character in a string
+ *
+ * @s: The string we need to locate a character
+ * @c: The character we need to locate
+ *
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c && *s != '\0')
+		s++;
+
+	if (*s == c)
+		return (s);
+
+	return ('\0');
+}
+
