d84684dd860c2afd7f5b7554b6ee1a494b0bd554
Falendrac <leger_franck@hotmail.fr>
2021-10-18 10:47:50 +0000

    Fix add a condition if: break for

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,14 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int length = 0, loop1, loop2;
 
 	for (loop1 = 0; *(s + loop1) != '\0' && loop1 <= loop2; loop1++)
+	{
 		for (loop2 = 0; *(accept + loop2) != '\0'; loop2++)
 			if (*(s + loop1) == *(accept + loop2))
 				length++;
+
+		if (*(accept + loop2) == '\0')
+			break;
+	}
 
 	return (length);
 }
