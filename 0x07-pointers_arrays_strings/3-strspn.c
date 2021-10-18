429ab56882be11b492204ec9ca1cc4bf930f2843
Falendrac <leger_franck@hotmail.fr>
2021-10-18 10:50:30 +0000

    Fix delete and rewrite conditions

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,18 +8,18 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int length = 0, loop1, loop2;
+	unsigned int loop1, loop2;
 
-	for (loop1 = 0; *(s + loop1) != '\0' && loop1 <= loop2; loop1++)
+	for (loop1 = 0; *(s + loop1) != '\0'; loop1++)
 	{
 		for (loop2 = 0; *(accept + loop2) != '\0'; loop2++)
 			if (*(s + loop1) == *(accept + loop2))
-				length++;
+				break;
 
 		if (*(accept + loop2) == '\0')
 			break;
 	}
 
-	return (length);
+	return (loop1);
 }
 
