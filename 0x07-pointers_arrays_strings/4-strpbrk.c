8ee40ff66d90eb93b00f9a57eae74c106631c1a1
Falendrac <leger_franck@hotmail.fr>
2021-10-18 09:04:55 +0000

    Add code source 4-strpbrk.c to 0x07 directory

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ *
+ * @s: The first string we search the first occurrence
+ * @accept: The bytes we search
+ *
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int loop1, loop2;
+
+	for (loop1 = 0; *(s + loop1) != '\0'; loop1++)
+		for (loop2 = 0; *(accept + loop2) != '\0'; loop2++)
+			if (*(s + loop1) == *(accept + loop2))
+				return ((s + loop1));
+
+	return (s);
+}
+
