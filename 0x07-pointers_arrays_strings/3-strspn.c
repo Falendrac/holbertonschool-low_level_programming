05c860fff5bb96ad1455260326013968f9fe96aa
Falendrac <leger_franck@hotmail.fr>
2021-10-18 08:23:00 +0000

    Add code source 3-strspn.c to 0x07 directory

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+/**
+ * _strspn - Gets the length of a prefix substring
+ *
+ * @s: Initial segment
+ * @accept: Characters we accept
+ *
+ * Return: length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0, loop1, loop2;
+
+	for (loop1 = 0; *(s + loop1) != '\0' && loop1 <= loop2; loop1++)
+		for (loop2 = 0; *(accept + loop2) != '\0'; loop2++)
+			if (*(s + loop1) == *(accept + loop2))
+				length++;
+
+	return (length);
+}
+
