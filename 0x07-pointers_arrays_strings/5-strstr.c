56b55c378e97e26d4832d8331c620db0d1a34917
Falendrac <leger_franck@hotmail.fr>
2021-10-18 11:50:46 +0000

    Fix move a condition outside a for

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,14 +11,14 @@ char *_strstr(char *haystack, char *needle)
 	int loop1, loop2;
 
 	for (loop1 = 0; *(haystack + loop1) != '\0'; loop1++)
+	{
 		for (loop2 = 0; *(needle + loop2) != '\0'; loop2++)
-			if (*(haystack + loop1 + loop2) == *(needle + loop2))
-			{
-				if (*(needle + loop2 + 1) == '\0')
-					return (haystack + loop1);
-			}
-			else
+			if (*(haystack + loop1 + loop2) != *(needle + loop2))
 				break;
+
+		if (*(needle + loop2) == '\0')
+			return (haystack + loop1);
+	}
 
 	return (0);
 }
