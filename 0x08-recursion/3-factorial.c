70de1f17ee1ba534ed11e47bf52b1dad7d9c1ab2
Falendrac <leger_franck@hotmail.fr>
2021-10-20 08:09:41 +0000

    Add source code 3-factorial.c to 0x08 directory

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - Return the factorial of a given number
+ *
+ * @n: The number we want the factorial
+ *
+ * Return: The factorial
+ */
+int factorial(int n)
+{
+	if (n > 0 && n != -1 && n < 13)
+		return (n * factorial(n - 1));
+	else if (n < 0 && n > -13)
+	{
+		n *= -1;
+		return ((n * factorial(n - 1)) * -1);
+	}
+	else if (n <= -13)
+		return (-1);
+	else
+		return (1);
+}
+
