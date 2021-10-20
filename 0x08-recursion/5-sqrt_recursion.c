853a478055948bade18e0cabcb992675d273ff3b
Falendrac <leger_franck@hotmail.fr>
2021-10-20 09:13:41 +0000

    Fix delete the u of squrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _squrt_recursion - Return the natural square root of a number
+ * _sqrt_recursion - Return the natural square root of a number
  *
  * @n: The number we want to square
  *
  * Return: -1 if n not have natural square root else the square
  */
-int _squrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
 	int i = 2;
 
@@ -26,14 +26,14 @@ int _squrt_recursion(int n)
 }
 
 /**
- * search_squrt - search the result of a squrt
+ * search_sqrt - search the result of a squrt
  *
  * @n: The number we want to square
  * @i: The first value we increment in the function
  *
  * Return: i
  */
-int search_squrt(int n, int i)
+int search_sqrt(int n, int i)
 {
 	if (i * i < n)
 		search_squrt(n, i + 1);
