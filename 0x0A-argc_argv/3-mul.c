eb074f5b25f3d745a66b27e3300ad5873622e8e0
Falendrac <leger_franck@hotmail.fr>
2021-10-22 08:24:31 +0000

    Add 3-mul.c to 0x0A directory

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers and print the result
+ *
+ * @argv: Number of arguments passed when the program is executed
+ * @argc: Array of pointers of Strings passed when the program is executed
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc == 1)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	result = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", result);
+
+	return (0);
+}
