a4e476afee24334d90fbd25817f107246be4d13a
Falendrac <leger_franck@hotmail.fr>
2021-10-22 07:20:19 +0000

    Add 0-whatsmyname.c to 0x0A directory

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print its name, followed by a new line
+ *
+ * @argc: Number of argument when the program is executed
+ * @argv: Array of pointers of string of arguments passed
+ * when the program is executed
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
