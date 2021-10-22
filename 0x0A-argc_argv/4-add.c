3112b27f84a930bad88cfe7fa9edb4eeab386379
Falendrac <leger_franck@hotmail.fr>
2021-10-22 10:37:53 +0000

    Fix 4-add.c, change a condition and add isnumber function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int isnumber(char *str);
+
 /**
  * main - Add positive numbers and prints the result
  *
@@ -16,7 +18,7 @@ int main(int argc, char *argv[])
 	if (argc > 1)
 	{
 		for (loop = 1; loop < argc; loop++)
-			if (*argv[loop] >= 48 && *argv[loop] <= 57)
+			if (isnumber(argv[loop]) == 1)
 				result += atoi(argv[loop]);
 			else
 			{
@@ -28,4 +30,24 @@ int main(int argc, char *argv[])
 	printf("%d\n", result);
 
 	return (0);
+}
+
+/**
+ * isnumber - Search if a string is a number
+ *
+ * @str: The string we search the number
+ *
+ * Return: 1 if is a number, otherwise 0
+ */
+int isnumber(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		else
+			str++;
+	}
+
+	return (1);
 }
