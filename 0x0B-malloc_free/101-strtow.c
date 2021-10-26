176828407c02b8d86aacad0465c8f66e39877d10
Falendrac <leger_franck@hotmail.fr>
2021-10-26 19:05:28 +0000

    Pass the +2 in the second malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,7 +22,7 @@ char **strtow(char *str)
 	if (numberWords == 0)
 		return (NULL);
 
-	aled = (char **)malloc(sizeof(char *) * numberWords + 2);
+	aled = (char **)malloc(sizeof(char *) * numberWords + 1);
 
 	if (aled == NULL)
 		return (NULL);
@@ -31,7 +31,7 @@ char **strtow(char *str)
 	{
 		if (*(str + i) != ' ')
 		{
-			*(aled + iAled) = (char *)malloc(sizeof(char) * strwlen((str + i)) + 1);
+			*(aled + iAled) = (char *)malloc(sizeof(char) * strwlen((str + i)) + 2);
 
 			if (*(aled + iAled) == NULL)
 			{
