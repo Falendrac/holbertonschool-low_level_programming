639030ea38d44b2f2b856070c259b13719b676e6
Falendrac <leger_franck@hotmail.fr>
2021-10-26 09:58:24 +0000

    Fix 0-create_array.c delete if c = null and add if str == null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,10 +17,10 @@ char *create_array(unsigned int size, char c)
 	if (size <= 0)
 		return (NULL);
 
-	if (c == NULL)
-		return (NULL);
-
 	str = malloc(sizeof(char) * size);
+
+	if (str == NULL)
+		return (NULL);
 
 	for (loop = 0; loop < size; loop++)
 	{
