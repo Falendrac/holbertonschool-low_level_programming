fb4a7d86fa8bee134919e1ace8e6d26076ee64b5
Falendrac <leger_franck@hotmail.fr>
2021-10-26 09:19:11 +0000

    Add 3-allo_grid.c to 0x0B directory

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Create a 2 dimensional array of integers
+ *
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int loopWidth, loopHeight;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	for (loopHeight = 0; loopHeight < height; loopHeight++)
+		*(grid + loopHeight) = malloc(sizeof(int) * width);
+
+
+	for (loopWidth = 0; loopWidth < width; loopWidth++)
+		for (loopHeight = 0; loopHeight < height; loopHeight++)
+			grid[loopHeight][loopWidth] = 0;
+
+
+	return (grid);
+}
