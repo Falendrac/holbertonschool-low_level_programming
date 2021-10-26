b9d05b0b6e2db3def86f2727f3664758512152fc
Falendrac <leger_franck@hotmail.fr>
2021-10-26 07:58:54 +0000

    Add 0-create_array.c to 0x0B directory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * create_array - Create an array of chars, and initializes
+ * it with a specific char
+ *
+ * @size: The size of the array of chars
+ * @c: The char we use to initialize the array
+ *
+ * Return: str
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int loop;
+	char *str;
+
+	str = malloc(size);
+
+	for (loop = 0; loop < size; loop++)
+	{
+		*(str + loop) = c;
+	}
+
+	return (str);
+}
