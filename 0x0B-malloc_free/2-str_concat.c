e1cad654b3de8cf233ab6f4c38bf54ff22c914b1
Falendrac <leger_franck@hotmail.fr>
2021-10-26 08:23:48 +0000

    Add 2-str_concat.c to 0x0B directory

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - Concatenates two strings
+ *
+ * @s1: The first string to concatenates
+ * @s2: The second string to concatenates
+ *
+ * Return: str
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int s1Length, s2Length, loop;
+
+	s1Length = _strlen(s1);
+	s2Length = _strlen(s2);
+
+	str = malloc(s1Length + s2Length);
+
+	for (loop = 0; loop < (s1Length + s2Length); loop++)
+	{
+		if (loop < s1Length)
+			*(str + loop) = *(s1 + loop);
+		else
+			*(str + loop) = *(s2 + loop - s1Length);
+	}
+
+	return (str);
+}
+
+/**
+ * _strlen - Return the length of a string
+ *
+ * @s: The String we want to know the length
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+
+	return (length);
+}
