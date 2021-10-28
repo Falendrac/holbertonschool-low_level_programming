825c9590f7687ca933c62415c20abdd7fefb53b5
Falendrac <leger_franck@hotmail.fr>
2021-10-28 13:31:22 +0000

    Add 100-realloc.c to 0x0C directory

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - Function that reallocates a memory block using malloc and free
+ * The pointer ptr has been free and the content is copy in a new pointer
+ *
+ * @ptr: The pointer we want to free and copy the content
+ * @old_size: The size of the old pointer
+ * @new_size: The size of the new pointer
+ *
+ * Return: new_ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+	int loop;
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return NULL;
+		return (new_ptr);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size <= 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+		return (NULL);
+
+	for (loop = 0; loop < new_size; loop++)
+		*(new_ptr + loop) = *(char *)(ptr + loop);
+
+	free(ptr);
+
+	return (new_ptr);
+}
