21a4ed2de573affa24f2fee6d9b6feef98fbd268
Falendrac <leger_franck@hotmail.fr>
2021-10-28 13:43:36 +0000

    Fix 100-realloc.c in a condition for equal 0 and add a free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size <= 0)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
@@ -36,7 +36,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	new_ptr = malloc(new_size);
 
 	if (new_ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
 	for (loop = 0; loop < new_size; loop++)
 		*((char *)new_ptr + loop) = *((char *)ptr + loop);
