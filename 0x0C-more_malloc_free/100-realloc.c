e9f463325ba46afd80fd6c2920e32a977616c9f7
Falendrac <leger_franck@hotmail.fr>
2021-10-28 13:40:55 +0000

    Fix 100-realloc.c to stay with a void pointer

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr;
+	void *new_ptr;
 	unsigned int loop;
 
 	if (ptr == NULL)
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	for (loop = 0; loop < new_size; loop++)
-		*(new_ptr + loop) = *((char *)ptr + loop);
+		*((char *)new_ptr + loop) = *((char *)ptr + loop);
 
 	free(ptr);
 
