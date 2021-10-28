db5c56c4ef09ce65f2e57a69c022c4c1f6597d1e
Falendrac <leger_franck@hotmail.fr>
2021-10-28 10:51:18 +0000

    Fix 2-calloc.c to support some type

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int loop;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,6 +21,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
+
+	for (loop = 0; loop < nmemb * size; loop++)
+		if (size == 1)
+			*((char *)ptr + loop) = 0;
+		else if (size == 2)
+			*((short *)ptr + loop) = 0;
+		else if (size == 4)
+			*((int *)ptr + loop) = 0;
 
 	return (ptr);
 }
