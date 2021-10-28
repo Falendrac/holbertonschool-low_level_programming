f9df1b532a6e920838a327c11dada56bceddc3f5
Falendrac <leger_franck@hotmail.fr>
2021-10-28 07:47:50 +0000

    Add 0-malloc_checked.C to 0x0B directory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Function that allocates memory
+ *
+ * @b: Number of bytes we want to allocate
+ *
+ * Return: ptr
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit (98);
+
+	return (ptr);
+}
