908fdf21be67fc66fa6d654d89a067282b9443ba
Falendrac <leger_franck@hotmail.fr>
2021-10-28 08:17:25 +0000

    Add 2-calloc.c to 0x0C directory

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Function that allocates memory for an array
+ *
+ * @nmemb: Number of element for the array
+ * @size: The size of type we want for the array
+ *
+ * Return: ptr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
+}
