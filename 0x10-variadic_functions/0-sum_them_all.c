64f4724d7d12126a9058084712a46103bc91a8e0
Falendrac <leger_franck@hotmail.fr>
2021-11-04 08:50:26 +0000

    Add 0-sum_them_all.c to 0x10 directory

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Returns the sum of all its parameters
+ *
+ * @n: Number of argument
+ *
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int loop;
+	va_list list_var;
+
+	va_start(list_var, n);
+
+	for (loop = 0; loop < n; loop++)
+		sum += va_arg(list_var, int);
+
+	va_end(list_var);
+	return (sum);
+}
