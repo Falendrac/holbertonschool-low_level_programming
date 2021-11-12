fd4bc19af88a15ebade2495769d8687ad14eef5c
Falendrac <leger_franck@hotmail.fr>
2021-11-12 10:31:16 +0000

    Update 3-add_node_end.c if strdup failed

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,6 +20,12 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	while (*(new_node->str + length) != '\0')
 		length++;
 
