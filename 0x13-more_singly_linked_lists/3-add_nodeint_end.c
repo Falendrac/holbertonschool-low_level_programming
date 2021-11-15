525a34a1dadf5e820587b5b7a5b3c6d2b0c74b88
Falendrac <leger_franck@hotmail.fr>
2021-11-15 11:02:14 +0000

    Add 3-add_nodeint_end.c to 0x13 directory

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Add a new node at the end of a listint_t list
+ *
+ * @head: The listint_t we add a new node at the end
+ * @n: Argument pass to be add in the new node
+ *
+ * Return: The adress of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *tmp_list;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		tmp_list = *head;
+		while (tmp_list->next != NULL)
+			tmp_list = tmp_list->next;
+
+		tmp_list->next = new_node;
+	}
+
+	return (new_node);
+}
