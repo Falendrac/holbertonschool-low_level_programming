68804f4ed9cfb4957bc7e145c836f8db5a24fd66
Falendrac <leger_franck@hotmail.fr>
2021-11-15 13:27:30 +0000

    Add 5-free_listint2.c to 0x13 directory

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free the memory of the entirely list
+ *
+ * @head: The linked list we want to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp_list;
+
+
+	while (*head != NULL)
+	{
+		tmp_list = *head;
+		*head = (*head)->next;
+		free(tmp_list);
+	}
+
+	*head = NULL;
+	head = NULL;
+}
