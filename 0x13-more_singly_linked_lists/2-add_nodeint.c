03dcfcd56487d48fa8c6b0c92245352a570c8dd7
Falendrac <leger_franck@hotmail.fr>
2021-11-15 10:58:54 +0000

    Add 2-add_nodeint.c to 0x13 directory

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Add a new node at the beginning
+ * of a listint_t list
+ *
+ * @head: The listint_t we add a new node
+ * @str: Argument pass to be add in the new node
+ *
+ * Return: The adress of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node, *tmp_list;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		tmp_list = *head;
+		*head = new_node;
+		new_node->next = tmp_list;
+	}
+
+	return (new_node);
+}
