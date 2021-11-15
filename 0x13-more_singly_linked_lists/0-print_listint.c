8d88b3dd4370f5d4534a1f9ef52c62107d3e8121
Falendrac <leger_franck@hotmail.fr>
2021-11-15 10:49:19 +0000

    Add 0-print_listint.c to 0x13 directory

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Print all the elements of a list_t list
+ *
+ * @h: The singly linked list we print all the elements
+ *
+ * Return: element
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t element = 0;
+	const listint_t *tmp_list;
+
+	tmp_list = h;
+	while (tmp_list != NULL)
+	{
+		element++;
+		printf("[%d]\n", tmp_list->n);
+		tmp_list = tmp_list->next;
+	}
+
+	return (element);
+}
