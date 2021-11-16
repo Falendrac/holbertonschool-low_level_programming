6d8005ce2a380162cd3f4b003b68e12cdda6a83b
Falendrac <leger_franck@hotmail.fr>
2021-11-16 12:34:42 +0000

    Add 101-print_listint_safe.c to 0x13 directory

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - Print all the elements of a listint_t list
+ * with safe exit
+ *
+ * @h: The singly linked list we print all the elements
+ *
+ * Return: element
+ */
+size_t print_listint_safe(const listint_t *h)
+{
+	size_t element = 0;
+	const listint_t *tmp_list;
+
+	tmp_list = h;
+	while (tmp_list != NULL)
+	{
+		element++;
+		printf("[%p] %d\n", (void *)tmp_list, tmp_list->n);
+		tmp_list = tmp_list->next;
+
+		if (tmp_list != NULL && tmp_list < tmp_list->next)
+		{
+			printf("-> [%p] %d\n", (void *)tmp_list->next, tmp_list->next->n);
+			exit(98);
+		}
+	}
+
+	return (element);
+}
