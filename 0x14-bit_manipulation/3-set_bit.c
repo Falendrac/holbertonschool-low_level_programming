115125e3c3d75713e7b7fc5b5b5280a184274f07
Falendrac <leger_franck@hotmail.fr>
2021-11-18 13:54:42 +0000

    Fix 3 tasks using bit operator for a better code

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,23 +1,25 @@
 #include "main.h"
 #include <limits.h>
 
+/**
+ * set_bit - Set the value of a bit to 1 at a given index
+ *
+ * @n: The number we want to get a bit
+ * @index: The index of the bit we want to get
+ *
+ * Return: The value of the bit at index index or -1 if an error occured
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int good;
-	unsigned int loop = 0;
 	unsigned long int bit = 1;
 
-	for (loop = 0; loop < index && bit < ULONG_MAX; loop++)
-		bit *= 2;
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
 
-	if ((bit + *n) < ULONG_MAX && loop == index)
-	{
-		*n += bit;
-		good = 1;
-	}
-	else
-		good = -1;
+	bit = 1 << (index);
+
+	*n += bit;
 
 
-	return (good);
+	return (1);
 }
