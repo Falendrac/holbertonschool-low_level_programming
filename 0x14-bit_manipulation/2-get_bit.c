b29418b40cc9549a6a1491fd339858f25bb18b6b
Falendrac <leger_franck@hotmail.fr>
2021-11-18 13:06:42 +0000

    Add a if condition when index is superior of 10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * get_bit - Return the value of a bit at a given index
+ *
+ * @n: The number we want to get a bit
+ * @index: The index of the bit we want to get
+ *
+ * Return: The value of the bit at index index or -1 if an error occured
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 	unsigned int loop = 0;
 	unsigned long int n_divided = n;
 
+
+	if (index > 10)
+		return (-1);
 
 	while (loop < index && n_divided != 0)
 	{
