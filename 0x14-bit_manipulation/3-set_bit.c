8286add760fba628ccbdf8ce80676c4a11f9a63e
Falendrac <leger_franck@hotmail.fr>
2021-11-18 13:07:48 +0000

    Revert "Update task 3 adding a description for the function"
    This reverts commit fc24a2606b51ff3c252c32a511c4d216a8cfd0cc.

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,14 +1,6 @@
 #include "main.h"
 #include <limits.h>
 
-/**
- * set_bit - Set the value of a bit to 1 at a given index
- *
- * @n: The integer we change a bit
- * @index: The index where we change the bit
- *
- * Return: 1 if it worked, or -1 if an error occured
- */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	int good;
