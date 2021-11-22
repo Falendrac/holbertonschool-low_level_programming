33199286cea9bb10435399049285abeb5ab1260c
Falendrac <leger_franck@hotmail.fr>
2021-11-22 11:33:56 +0000

    Fix task 0, change type of 2 variable

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd;
-	size_t numberWrite, numberRead;
+	int numberWrite, numberRead;
 	char *buf;
 
 	if (filename == NULL)
