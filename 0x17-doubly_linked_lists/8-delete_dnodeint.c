0ce51e67a3f1f7190a479187469081325144677b
Falendrac <leger_franck@hotmail.fr>
2022-01-06 10:12:58 +0000

    Add task 8 to 0x17 directory

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Delete at the index in the dlistint list.
+ *
+ * @head: The pointer of pointer of dlistint list
+ * @index: The index where we delete a node
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int idx = 0;
+
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL && idx != index)
+		{
+			current = current->next;
+			idx++;
+		}
+		if (idx == 0)
+		{
+			*head = (*head)->next;
+			if (*head != NULL)
+				(*head)->prev = NULL;
+			free(current);
+		}
+		else if (idx == index)
+		{
+			current->prev->next = current->next;
+			current->next->prev = current->prev;
+			free(current);
+		}
+		else if ((idx + 1) == index)
+		{
+			current->prev->next = NULL;
+			free(current);
+		}
+		else
+			return (-1);
+	}
+	return (1);
+}
