95a1b055904c2545b746380f3c15f4c9c4247333
Falendrac <leger_franck@hotmail.fr>
2022-01-06 10:01:53 +0000

    Add task 7 to 0x17 directory

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,43 +12,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
-	dlistint_t *current;
+	dlistint_t *new, *current;
 	unsigned int current_idx = 0;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-
 	if (*h == NULL)
 		*h = new;
 	else
 	{
 		current = *h;
-
 		while (current->next != NULL && current_idx != idx)
 		{
 			current = current->next;
 			current_idx++;
 		}
-
 		if (idx == 0)
 		{
 			new->next = current;
 			*h = new;
 		}
-		else
+		else if (current_idx == idx)
 		{
 			new->prev = current->prev;
 			new->next = current;
 			current->prev->next = new;
 			current->prev = new;
 		}
+		else if ((current_idx + 1) == idx)
+		{
+			current->next = new;
+			new->prev = current;
+		}
+		else
+			free(new);
 	}
-
 	return (new);
 }
