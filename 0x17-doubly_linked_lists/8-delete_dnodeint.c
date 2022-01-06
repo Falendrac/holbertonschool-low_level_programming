b0408aea0b9d8b4d3d947678ee160e67745430cd
Falendrac <leger_franck@hotmail.fr>
2022-01-06 12:14:45 +0000

    Fix task 8 undo last change et add a new condition if head is null

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,9 +15,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int idx = 0;
 
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 	{
-		return (1);
+		return (-1);
 	}
 	else
 	{
