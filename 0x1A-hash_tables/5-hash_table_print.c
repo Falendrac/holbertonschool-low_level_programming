d0793bf7b9d44af1c44388a60993de86770c82bb
Falendrac <leger_franck@hotmail.fr>
2022-01-27 14:19:25 +0000

    Add task 5 to 0x1A

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print the current hash table
+ *
+ * @ht: The hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *current;
+	int first_print = 0;
+
+	if (ht)
+	{
+		printf("{");
+		for (idx = 0; idx < ht->size; idx++)
+		{
+			if (ht->array[idx])
+			{
+				current = ht->array[idx];
+				if (first_print)
+					printf(", ");
+
+				while (current)
+				{
+					first_print = 1;
+					printf("'%s': '%s'", current->key, current->value);
+					current = current->next;
+				}
+
+			}
+		}
+		printf("}\n");
+	}
+}
