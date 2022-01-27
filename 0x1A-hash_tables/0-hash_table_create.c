2e50b5a7e7029fcfaa474fc10955c9ab5fa82beb
Falendrac <leger_franck@hotmail.fr>
2022-01-27 09:57:00 +0000

    Adding the task 0 to 0x1A

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - Create a new hash table of size "size"
+ *
+ * @size: Size of the hash table
+ *
+ * Return: The new hash table, or NULL if the malloc failled
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+
+	if (size == 0)
+		return (NULL);
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->size = size;
+	new->array = calloc(sizeof(hash_node_t), size);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	return (new);
+}
