cbd41fd6e8c5d7c05437be7c8eb41e78fcc510e6
Falendrac <leger_franck@hotmail.fr>
2022-01-28 13:46:58 +0000

    Delete the function new node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,45 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * new_node - Create a new node hash_node_t
- *
- * @key: the key
- * @value: the value of the key
- *
- * Return: NULL if malloc failled, or the new node
- */
-hash_node_t *new_node(const char *key, const char *value)
-{
-	hash_node_t *new;
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (NULL);
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->value = strdup(value);
-	if (new->value == NULL)
-	{
-		free(new->key);
-		free(new);
-		return (NULL);
-	}
-	new->next = NULL;
-
-	return (new);
-}
-
-/**
  * hash_table_set - Add an element to the hash table
  *
  * @ht: the hashe table
  * @key: key to insert in the hash table, can not be an empty string
  * @value: value of the key can be an empty string
- *
  * Return: HASH_SET_FAIL (1) if ht, ht->array an key is null or malloc failed
  * otherwise return HASH_SET_SUCCESS (0)
  */
@@ -49,11 +15,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t **array = ht->array;
 	hash_node_t *new = NULL, *current;
 
-	/* Test if ht, array and key is null */
 	if (!ht || !ht->array || !key || strcmp(key, "") == 0 || !value)
 		return (HASH_SET_FAIL);
 
-	index = key_index((unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	current = array[index];
 	while (current)
 	{
@@ -67,13 +32,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
-
-	new = new_node(key, value);
+	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (HASH_SET_FAIL);
-
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (HASH_SET_FAIL);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (HASH_SET_FAIL);
+	}
 	new->next = array[index];
 	array[index] = new;
-
 	return (HASH_SET_SUCCESS);
 }
