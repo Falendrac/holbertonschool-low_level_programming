d355c0739fb30e19de01a4eb22fc45fa2343d301
Falendrac <leger_franck@hotmail.fr>
2022-01-28 08:14:28 +0000

    Update 6 for adding description

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - Free all the memory set for the hash table
+ *
+ * @ht: hash table
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int idx;
