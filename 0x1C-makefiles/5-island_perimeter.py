9d8c266631f3fc8ad4695546c1ff4db1391135b3
Falendrac <leger_franck@hotmail.fr>
2022-03-17 15:28:42 +0100

    Fix the diagonal count.

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -26,10 +26,47 @@ def island_perimeter(grid):
 
     for x in range(len(grid)):
         if 1 in grid[x]:
-            height += 1
             for y in range(len(grid[1])):
                 if grid[x][y] == 1:
-                    tmp += 1
+                    if (x == 0 and y == 0) and \
+                            (grid[x + 1][y] == 1 or grid[x][y + 1] == 1):
+                        tmp += 1
+                    elif (x > 0 and x < len(grid) - 1 and y == 0) and \
+                            (grid[x + 1][y] == 1 or grid[x - 1][y] == 1
+                                or grid[x][y + 1] == 1):
+                        tmp += 1
+                    elif (x > 0 and x == len(grid) - 1 and y == 0) \
+                            and (grid[x - 1][y] == 1 or grid[x][y + 1] == 1):
+                        tmp += 1
+                    elif (x == 0 and y > 0 and y < len(grid) - 1) and (
+                            grid[x + 1][y] == 1 or
+                            grid[x][y + 1] == 1 or grid[x][y - 1]
+                            ):
+                        tmp += 1
+                    elif (x == 0 and y > 0 and y == len(grid) - 1) \
+                            and (grid[x + 1][y] == 1 or grid[x][y - 1]):
+                        tmp += 1
+                    elif (x > 0 and x < len(grid) - 1 and
+                            y != 0 and y < len(grid) - 1) and \
+                            (grid[x + 1][y] == 1 or grid[x - 1][y] == 1
+                                or grid[x][y + 1] == 1 or grid[x][y - 1]):
+                        tmp += 1
+                    elif (x > 0 and x == len(grid) - 1 and
+                            y != 0 and y < len(grid) - 1) and (
+                                grid[x - 1][y] == 1 or
+                                grid[x][y + 1] == 1 or grid[x][y - 1]):
+                        tmp += 1
+                    elif (x > 0 and x < len(grid) - 1 and
+                            y != 0 and y == len(grid) - 1) and (
+                                grid[x + 1][y] == 1 or
+                                grid[x - 1][y] == 1 or grid[x][y - 1]):
+                        tmp += 1
+                    elif (x > 0 and x == len(grid) - 1 and
+                            y != 0 and y == len(grid) - 1) and\
+                            (grid[x - 1][y] == 1 or grid[x][y - 1]):
+                        tmp += 1
+            if tmp != 0:
+                height += 1
             if tmp > width:
                 width = tmp
             tmp = 0
