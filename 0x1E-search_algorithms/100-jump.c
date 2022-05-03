4e41194f2b0c0e51363e398075387c7f5183a732
Falendrac <leger_franck@hotmail.fr>
2022-05-03 12:09:28 +0200

    Fix 100 to try to pass the checker.

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,7 +16,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t prev = 0;
 	size_t step = sqrt(size);
 
-	if (array == NULL)
+	if (array == NULL || array[0] > value)
 		return (-1);
 
 	printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
