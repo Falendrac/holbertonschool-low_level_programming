43f0a6252cfb97e40af8e9221e54a848fc5bb6fd
Falendrac <leger_franck@hotmail.fr>
2022-05-04 11:41:55 +0200

    Fix 106 better returns.

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -40,8 +40,11 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	}
 
 	if (tmpStart)
+	{
 		printf("Value checked at index [%ld] = [%d]\n",
 		tmpStart->index, tmpStart->n);
+		return (tmpStart);
+	}
 
-	return (tmpStart);
+	return (NULL);
 }
